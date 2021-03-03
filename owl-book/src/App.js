30d8c58f13eca0f41d95b889f77af5cb42f70296
Alejandro Francés <afrances@serikat.es>
2021-03-03 09:21:59 +0100

    añadida app

---
+++ owl-book/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import OwlHeader from './componets/OwlHeader/OwlHeader';
+
+function App() {
+  return (
+    <div className="App">
+   APPP
+    </div>
+  );
+}
+
+export default App;
