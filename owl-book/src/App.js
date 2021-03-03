d00dd2e01aa705e29870ffb9849065e71bcd5769
Cristian <crcaraballo25@gmail.com>
2021-03-03 16:36:33 +0100

    clase 02

---
+++ owl-book/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import OwlHeader from './componets/OwlHeader/OwlHeader';
+
+function App() {
+  return (
+    <div className="App">
+      Owl book
+    </div>
+  );
+}
+
+export default App;
