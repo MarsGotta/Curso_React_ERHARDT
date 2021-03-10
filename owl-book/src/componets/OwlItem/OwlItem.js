753c422ce935dca46b7dfd4eaea04edf4887a3fa
Mars Gotta <you@example.com>
2021-03-10 19:43:14 +0100

    fix: icon changed

--- owl-book/src/componets/OwlItem/OwlItem.js
+++ owl-book/src/componets/OwlItem/OwlItem.js
@@ -1,4 +1,5 @@
 import OwlIcon from '../OwlIcon/OwlIcon'
+import OwlRating from '../OwlRating/OwlRating'
 import './OwlItem.css';
 
 function OwlItem({ src, title, author, stars, liked, genre }) {
@@ -7,8 +8,8 @@ function OwlItem({ src, title, author, stars, liked, genre }) {
       <img src={src} alt={title} />
       <h4>{title}</h4>
       <h5>{author}</h5>
-      <div>Estrellas: {stars}</div>
-      <OwlIcon name="heart" liked={liked} />
+      <div><OwlRating stars={stars} /></div>
+      <OwlIcon name="heart" liked={liked} stroke="aliceblue" />
       <p>Género: {genre}</p>
     </article>
   );
