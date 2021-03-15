ef225d50a5d404885fe662e7721d90061302b0b0
BARRERO IBAÑEZ <abarrero@serikat.es>
2021-03-15 19:10:25 +0100

    filter

--- owl-book/src/App.js
+++ owl-book/src/App.js
@@ -1,15 +1,38 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import OwlLiteList from './componets/OwlLiteList/OwlLiteList';
 import OwlTabList from './componets/OwlTabList/OwlTabList';
-import {generosList, bookList, lastBookList} from './mocks';
+import {useAPI} from './core';
 
 function App() {
-  return (
-    <div className="App">
+
+  const [list, setList] = useState([]);
+  const [genres, setGenres] = useState([]);
+
+  const booksResponse = useAPI('http://localhost:4444/books');
+  const genresResponse = useAPI('http://localhost:4444/genres');
+  const booksData = booksResponse?.response;
+  const genresData = genresResponse?.response;
+
+  useEffect(() => {
+    setList(booksData);
+    setGenres(genresData)
+  }, [booksData, genresData])
+
+  const renderLoading = () => booksData?.isLoading && <p>Cargando...</p>;
+  const renderMain = () => !booksData?.isLoading && (
+    <>
       <h2>Grandes clásicos para ti</h2>
-      <OwlTabList generosList= {generosList} bookList={bookList}/>
-      <OwlLiteList bookList={lastBookList} />
-    </div>
+      <OwlTabList genreList={genres} bookList={list} />
+      <OwlLiteList bookList={list} />
+    </>
+  )
+
+  return (
+    <main className="App">
+      {renderLoading()}
+      {renderMain()}
+    </main>
   );
 }
 
--- owl-book/src/componets/OwlTabList/OwlTabList.js
+++ owl-book/src/componets/OwlTabList/OwlTabList.js
@@ -2,35 +2,28 @@ import { useState } from 'react';
 import './OwlTabList.css';
 import OwlItem from '../OwlItem/OwlItem';
 
-function OwlTabList({generosList, bookList}) {
+function OwlTabList({genreList, bookList}) {
     const [filterList, setFilterList] = useState(bookList);
 
     const handleClick = id => {
-        const list = [...bookList].filter((item) => item.generoId === id && item);
+        const list = [...bookList].filter((item) => (item.genreId === id || id === 1) && item);
         setFilterList(list);
-    }
-
-    const handleClear = () => {
-        setFilterList(bookList);
     }
 
     return (
         <>
             <ul className="tab">
-                {generosList.map(({id, name}, key) => {
+                {genreList.map(({id, name}, key) => {
                     return  <li className="tab-link" key={key}>
                                 <button onClick={() => handleClick(id)}>{name}</button>
                             </li>   
                 })}
-                <li className="tab-link">
-                    <button onClick={() => handleClear()}>Limpiar</button>
-                </li>
             </ul>
             <div className="container">
                 <ul>
                     {filterList.map((item, key) => {
-                        const {src, title, author, stars, liked, generoId} = item
-                        return <OwlItem key={key} src={src} title={title} author={author} stars={stars} liked={liked} genero={generoId}/>
+                        const {src, title, author, stars, liked, genreId} = item
+                        return <OwlItem key={key} src={src} title={title} author={author} stars={stars} liked={liked} genero={genreId}/>
                     })}
                 </ul>
             </div>
